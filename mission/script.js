313fc1992feebf4ddf8215e9a26a201ce13260e3
«Dima_Mirontsov» <miron2098@mail.ru>
2023-02-14 14:10:32 +0300

    add delet box

--- mission/script.js
+++ mission/script.js
@@ -5,11 +5,8 @@ const todoList = document.querySelector('.todo-list')
 const todoCompleted = document.querySelector('.todo-completed')
 const headerButton = document.querySelector('.header-button')
 
-	let toDoData = []
-	localStorage.setItem('item',JSON.stringify(toDoData))
-	toDoData = JSON.parse(localStorage.getItem('item'))
-	console.log(toDoData);
-
+let toDoData = localStorage.getItem('item') ? JSON.parse(localStorage.getItem('item')) : []
+console.log(toDoData);
  
 const render = function(){
 	todoList.innerHTML = ''
@@ -39,8 +36,13 @@ const render = function(){
 //удаление по корзине
 	li.querySelector('.todo-remove').addEventListener('click',function(){
 		li.classList.remove('todo-item')
-		li.innerHTML = ''
+		li.innerHTML=''
+		toDoData.splice('li',1)
+		localStorage.removeItem('item')
 	})
+
+	localStorage.setItem('item', JSON.stringify(toDoData))
+
 	})
 }
 
@@ -68,3 +70,4 @@ todoControl.addEventListener('submit',function(event){
 	
 })
 
+render(toDoData) //Чтоб после загрузки страницы мы 1 раз все отрисовали)
